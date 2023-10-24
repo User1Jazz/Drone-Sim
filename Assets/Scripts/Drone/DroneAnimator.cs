97a05b85b7bfa72ebd5db5724ac6570c153c9c90
User1Jazz <phoenix.productions222@gmail.com>
2023-10-24 14:56:01 +0100

    Finished drone controller

---
+++ Assets/Scripts/Drone/DroneAnimator.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneAnimator : MonoBehaviour
+{
+    [SerializeField] DroneController droneControllerScript;
+    [SerializeField] Transform droneMainTransform;
+
+    // Rotation animation vars
+    float tiltAmountForward = 0f;
+    float tiltVelocityForward = 0f;
+
+    float tiltAmountRight = 0f;
+    float tiltVelocityRight = 0f;
+
+    float yAxis = 0;
+
+
+    // Propeller animation vars
+    [SerializeField] List<Transform> clockwisePropellers;
+    [SerializeField] List<Transform> counterClockwisePropellers;
+    public float maxPropellerRPM = 20000f;
+    float rotationRate = 1f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        HandleRotation();
+        HandleMotors();
+    }
+
+    
+    void HandleRotation()
+    {
+        tiltAmountForward = Mathf.SmoothDamp(tiltAmountForward, droneMainTransform.eulerAngles.x + 20 * droneControllerScript.cyclic.x, ref tiltVelocityForward, 0.1f);     // Calculate forward tilt
+        tiltAmountRight = Mathf.SmoothDamp(tiltAmountRight, droneMainTransform.eulerAngles.z + 20 * droneControllerScript.cyclic.y, ref tiltVelocityRight, 0.1f);           // Calculate side tilt
+
+        yAxis += droneControllerScript.pedals * droneControllerScript.yawSpeed * Time.deltaTime;                                                                            // Calculate yaw
+
+        Vector3 rotation = new Vector3(tiltAmountForward, yAxis, tiltAmountRight);                                                                                          // Create a rotation vector
+
+        transform.rotation = Quaternion.Euler(rotation);                                                                                                                    // Apply rotation
+    }
+
+    void HandleMotors()
+    {
+        foreach (Transform propeller in clockwisePropellers)
+        {
+            propeller.Rotate(Vector3.forward, maxPropellerRPM * rotationRate);
+        }
+        foreach (Transform propeller in counterClockwisePropellers)
+        {
+            propeller.Rotate(Vector3.forward, maxPropellerRPM * -rotationRate);
+        }
+    }
+}
--- Assets/Scripts/Drone/DroneController.cs
+++ Assets/Scripts/Drone/DroneController.cs
@@ -2,33 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum ControlType{
-    AssistON, AssistOFF
-};
 
-//[RequireComponent(typeof(DroneRB))]
+[RequireComponent(typeof(Rigidbody))]
 public class DroneController : MonoBehaviour
 {
-    public Vector2 cyclic;                                          // Pitch and roll
-    public float pedals;                                            // Yaw
-    public float throttle;                                          // Throttle (vertical force)
-    [Tooltip("Degrees per second")] public float yawSpeed = 15f;    // Yaw power
-    [Tooltip("Degrees per second")] public float pitchSpeed = 15f;  // Pitch speed (deg/sec)
-    [Tooltip("Degrees per second")] public float rollSpeed = 15f;   // Max roll (deg/sec)
-    public ControlType controlType = ControlType.AssistON;
+    public Vector2 cyclic;                                         // Pitch and roll
+    public float pedals;                                           // Yaw
+    
+    [Tooltip("Yaw Speed (Deg/s)")] public float yawSpeed = 15f;    // Yaw power
+    [Tooltip("Pitch Speed (m/s)")] public float pitchSpeed = 15f;  // Pitch speed (deg/sec)
+    [Tooltip("Roll Speed (m/s)")] public float rollSpeed = 15f;    // Max roll (deg/sec)
 
-    [Header("Assist ON Parameters")]
-    [Tooltip("Degrees")] public float maxPitch = 30f;               // Max pitch angle (deg)
-    [Tooltip("Degrees")] public float maxRoll = 30f;                // Max roll angle (deg)
+    Rigidbody rb;
+    public float motorPower = 5f;
+    public float throttle;                                         // Throttle (vertical force)
 
     //[Header("Assist OFF Parameters")]
 
-    void Update()
+    void Start()
     {
-        ManualControls();
+        rb = GetComponent<Rigidbody>();
     }
 
-    void ManualControls()
+    void FixedUpdate()
+    {
+        Hover();
+
+        Throttle(throttle * motorPower);
+        Nick(pitchSpeed * cyclic.x);
+        Roll(rollSpeed * cyclic.y);
+        Yaw(yawSpeed * pedals);
+    }
+
+    void Update()
+    {
+        HandleManualInput();
+    }
+
+    void  HandleManualInput()
     {
         //cyclic.x = Input.GetAxis("Vertical");
         //cyclic.y = -Input.GetAxis("Horizontal");
@@ -85,4 +96,33 @@ public class DroneController : MonoBehaviour
             throttle = 0f;
         }
     }
+
+    void Hover()
+    {
+        rb.AddRelativeForce(Vector3.up * rb.mass * Physics.gravity.magnitude);
+    }
+
+    void Throttle(float force)
+    {
+        rb.AddRelativeForce(Vector3.up * rb.mass * Physics.gravity.magnitude * force);
+    }
+
+    void Nick(float force)
+    {
+        rb.AddRelativeForce(Vector3.forward * force);
+    }
+
+    void Roll(float force)
+    {
+        rb.AddRelativeForce(-Vector3.right * force);
+    }
+
+    void Yaw(float force)
+    {
+        Quaternion deltaRotation = Quaternion.Euler(new Vector3(0f, force, 0f) * Time.fixedDeltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
+    }
+
+    
+    
 }
